f0cdfb32d81790dff92b8064445158a652565112
Akshay Saini <anonyxhappie@cloud.anonyx.lan>
2017-03-01 20:38:00 +0530

    Final Update-1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,43 @@
-var QH_ARRAY = new Array('LVM', 'Quota', 'LDAP', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q');
-var selectedQuestion;
-var countDownDate = new Date("Feb 21, 2017 21:00:00").getTime();
+/**
+* -Feb 15, 2017 15:30:00
+* -Coded by anonyxhappie - Akshay Saini
+* -as a project of CBitss
+**/
+
+var QH_ARRAY = new Array();
+var QQ_ARRAY = new Array();
+
+/**
+* This module is for getting the questions and start date and time of exam.1
+**/
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function(){
+	if(this.readyState == 4 && this.status == 200) {
+		xmlParser(this);
+	} 
+};
+xhttp.open("GET", "xml/exam.xml", true);
+xhttp.send();
+var dateString="";
+function xmlParser(xml){
+	var xmlDoc = xml.responseXML;
+	var x = xmlDoc.documentElement.childNodes;
+	dateString = x[1].childNodes[0].nodeValue;	
+	//document.write(dateString);
+	for(var i = 0; i < 17; i++){
+		var qhead = xmlDoc.getElementsByTagName("qhead")[i].childNodes[0].nodeValue;
+		var question = xmlDoc.getElementsByTagName("question")[i].childNodes[0].nodeValue;		
+		QH_ARRAY.push(qhead);	
+		QQ_ARRAY.push(question);
+		var ss = 'p'+(i+1);
+		document.getElementById(ss).innerHTML = qhead;
+		document.getElementById(ss).value = qhead;
+	}
+}
+/**
+* This module is for the Timer.
+**/
+var countDownDate = new Date("Mar 02, 2017 18:00:00").getTime();
 var x = setInterval(function() {
     var now = new Date().getTime();
     var distance = countDownDate - now;
@@ -15,14 +52,27 @@ var x = setInterval(function() {
     }
 }, 1000);
 
+/**
+* This module is for style display of center div.
+**/
 function hide(x){
 	document.getElementById('review1').style.display="none";
 	document.getElementById('review2').style.display="block";
-	return x.value;
+	document.getElementById('qh').innerHTML = x.value;
+	document.getElementById('question').innerHTML = QQ_ARRAY[QH_ARRAY.indexOf(x.value)];
 }
-
 function show(x){	
 	document.getElementById('review2').style.display="none";
 	document.getElementById('review1').style.display="block";
 }
+
+/**
+* This module will submit and reboot the system.
+**/
+function reboot(){
+	
+}
+
+
+
 
--- php/db_connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$con = mysqli_connect("localhost","root","q","rhel_portal");
-	if ($con->connect_error) { 
-		die("Connection failed: " . $con->connect_error); 
-	}
-?>
